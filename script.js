2292d9e12c37dbc98d50c35a91e3d52b3b605725
maxime.polese <maxime.polese@le-camus-numerique.fr>
2023-10-10 13:42:59 +0200

    Affichage des borders

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 console.log('JS chargé');
 
-let size = 3;
+let size = 5;
 
 function getData(size, ex) {
     return data[0][size][ex];
 }
 
-let labyrinthe = getData(size.toString(), 'ex-0');
+let labyrinthe = getData(size.toString(), 'ex-1');
 console.log(labyrinthe);
 
 function displayLabyrinthe(labyrinthe) {
@@ -14,26 +14,48 @@ function displayLabyrinthe(labyrinthe) {
     labyrinthe.forEach(function (obj) {
         if (obj.entrance === true) {
             let entranceDiv = $('<div>');
-            entranceDiv.attr('class', 'entrance').text('entrance');
+            entranceDiv.attr('class', 'entrance');
+            displayBorder(obj, entranceDiv);
             $('main').append(entranceDiv);
         } else if (obj.exit === true) {
             let exitDiv = $('<div>');
-            exitDiv.attr('class', 'exit').text('exit');
+            exitDiv.attr('class', 'exit');
+            displayBorder(obj, exitDiv);
             $('main').append(exitDiv);
         } else {
             let div = $('<div>');
-            div.text('Case1');
+            displayBorder(obj, div);
             $('main').append(div);
         }
     });
 }
 
-displayLabyrinthe(labyrinthe);
-
-function displayBorder() {
-
+function displayBorder(obj, div) {
+    if (obj.walls[0] === true) {
+        div.css({
+            'border-top': 'solid 1px',
+            'border-top-color': 'red'
+        });
+    }
+    if (obj.walls[1] === true) {
+        div.css({
+            'border-right': 'solid 1px',
+            'border-right-color': 'red'
+        });
+    }
+    if (obj.walls[2] === true) {
+        div.css({
+            'border-bottom': 'solid 1px',
+            'border-bottom-color': 'red'
+        });
+    }
+    if (obj.walls[3] === true) {
+        div.css({
+            'border-left': 'solid 1px',
+            'border-left-color': 'red'
+        });
+    }
 }
 
+displayLabyrinthe(labyrinthe);
 
-// let row = $('<tr>');
-// let col = $('<td>');
