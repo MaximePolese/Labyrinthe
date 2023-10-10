260ab4357c26c77db3a16a71546f7e0bc2e4de33
maxime.polese <maxime.polese@le-camus-numerique.fr>
2023-10-10 11:39:06 +0200

    Affichage du labyrinthe en fonction du nombre de cases

--- script.js
+++ script.js
@@ -1,18 +1,39 @@
 console.log('JS chargé');
 
+let size = 3;
+
 function getData(size, ex) {
     return data[0][size][ex];
 }
 
-let labyrinthe = getData('3', 'ex-0');
+let labyrinthe = getData(size.toString(), 'ex-0');
 console.log(labyrinthe);
 
 function displayLabyrinthe(labyrinthe) {
-    labyrinthe.forEach(function () {
-        let div = $('<div>');
-        div.text('Case1');
-        $('body').append(div);
+    $('main').attr('style', ('width:' + (size * 100) + 'px'));
+    labyrinthe.forEach(function (obj) {
+        if (obj.entrance === true) {
+            let entranceDiv = $('<div>');
+            entranceDiv.attr('class', 'entrance').text('entrance');
+            $('main').append(entranceDiv);
+        } else if (obj.exit === true) {
+            let exitDiv = $('<div>');
+            exitDiv.attr('class', 'exit').text('exit');
+            $('main').append(exitDiv);
+        } else {
+            let div = $('<div>');
+            div.text('Case1');
+            $('main').append(div);
+        }
     });
 }
 
-displayLabyrinthe(data);
+displayLabyrinthe(labyrinthe);
+
+function displayBorder() {
+
+}
+
+
+// let row = $('<tr>');
+// let col = $('<td>');
